cf70a89f713820413193ba31a57578163e154cd0
SamurayDev <gabriel.m.s2100@gmail.com>
2025-05-09 15:54:37 -0300

    feat: adicionado ao js ação de fechar menu ao escolher alguma seção

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const btn = document.querySelector('.menu-btn');
 btn.addEventListener('click', () => {
   document.body.classList.toggle('menu-aberto');
+
+});
+
+const btnmenu = document.querySelector('.menu-mobile li a');
+btnmenu.addEventListener('click', () =>     {
+  document.body.classList.toggle('menu-aberto');
 });
